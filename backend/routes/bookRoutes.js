076137721bbadd081ef2f90057d59b1a7c444f56
Kartik Bhardwaj <kbrupc2020@gmail.com>
2025-04-08 12:22:06 +0530

    Delete backend directory

--- backend/routes/bookRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllBooks } = require('../controllers/bookController');
-
-
-router.get('/', getAllBooks);
-
-module.exports = router;
