b4bea6b709d13449459bf16582bfde482530b8cb
demo_practice <kartikbhardwaj1111>
2025-04-08 22:44:14 +0530

    Add POST API endpoint for creating a book

--- backend/controllers/bookControllers.js
+++ backend/controllers/bookControllers.js
@@ -1,4 +1,6 @@
 const Book = require('../models/Book');
+
+// Get all books
 const getAllBooks = async (req, res) => {
   try {
     const books = await Book.find();
@@ -7,4 +9,17 @@ const getAllBooks = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
-module.exports = { getAllBooks };
+
+// Create a new book (POST)
+const createBook = async (req, res) => {
+  try {
+    const { title, author, description } = req.body;
+    const newBook = new Book({ title, author, description });
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = { getAllBooks, createBook };
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllBooks } = require('../controllers/bookControllers');
+const { getAllBooks, createBook } = require('../controllers/bookControllers');
 
-
+// GET route - all books
 router.get('/', getAllBooks);
+
+// POST route - create book
+router.post('/', createBook);
 
 module.exports = router;
