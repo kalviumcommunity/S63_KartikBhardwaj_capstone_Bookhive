efd6bb205a725c847d110d9efb96083da81e46f4
demo_practice <kartikbhardwaj1111>
2025-04-08 12:35:19 +0530

    GET API used

---
+++ backend/controllers/bookControllers.js
@@ -0,0 +1,10 @@
+const Book = require('../models/Book');
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+module.exports = { getAllBooks };
